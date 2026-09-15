561500fbaa3f27930a270a27e50d26fe794e71f8
hanhyesu <heasoo88@gmail.com>
2026-09-16 01:10:17 +0900

    migration: spring boot 3 -> spring boot 4 migration

--- src/main/java/hongik/Todoing/global/config/RestTemplateConfig.java
+++ src/main/java/hongik/Todoing/global/config/RestTemplateConfig.java
@@ -4,7 +4,7 @@ import io.micrometer.core.instrument.binder.httpcomponents.hc5.PoolingHttpClient
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.restclient.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
